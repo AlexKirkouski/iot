f5d6d2b68998606ba5730fb427b4d64870a6a883
yra <yra_bos@mail.ru>
2019-08-05 18:25:21 +0300

    SEND UDP TIME

--- src/main/java/mite/UDPServer.java
+++ src/main/java/mite/UDPServer.java
@@ -201,13 +201,13 @@ public class UDPServer extends MonitorServer {
         String cLab = revers(cPacket,0,2);
         if (!cLab.equals("FFFF")) return false;
         String cId = Long.toString(Long.parseLong(revers(cPacket,8,12),16));
-        byte[] data;
+        byte[] data = {1,0,2,4};
         InetAddress pAddress = dPacket.getAddress();
         Integer pPort = dPacket.getPort();
         print("LABEL TIME, " + cId + "... " + pAddress.toString() + ":" + pPort.toString());
         try {
             Long time = System.currentTimeMillis() / 1000L + 3600 * 2;
-            data = time.toString().getBytes();
+//            data = time.toString().getBytes();
             DatagramPacket dp = new DatagramPacket(data, data.length,pAddress ,pPort);
             serverSocket.send(dp);
         } catch (IOException e) {
