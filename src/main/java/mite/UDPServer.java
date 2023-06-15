e320c8d3f7a8a1c3b8e23957335c8e524753b762
AlexKirkouski <alex.kirkouski@gmail.com>
2023-06-15 13:10:31 +0100

    Small fix

--- src/main/java/mite/UDPServer.java
+++ src/main/java/mite/UDPServer.java
@@ -366,7 +366,7 @@ public class UDPServer extends MonitorServer {
                         receivedString = "length : " + read + ", data : " + bytesToHex(buf);
                     } else {
                         final Socket socket = serverTCPSocket.accept();
-                        socket.setSoTimeout(60000);
+                        socket.setSoTimeout(600000);
                         print("TCP CONNECTED : " + socket.getInetAddress());
                         TCPSocket rtcpSocket = new TCPSocket(socket);
                         tcpSockets.add(rtcpSocket);
